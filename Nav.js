6cdad873ece18123a82a3a057b23c6a5fd17869c
Bongani Ntshangase <77122273+bookjukies@users.noreply.github.com>
2023-01-10 23:48:47 +0200

    nav links

---
+++ Nav.js
@@ -0,0 +1,13 @@
+const AdminNav = () =>{
+    return(
+        <div className="w-1/4">
+            <a className="block" href="home">Analytics</a>
+            <a className="block" href="orders">Orders</a>
+            <a className="block" href="discounts">Discounts</a>
+            <a className="block" href="Upload">Upload</a>
+            <a className="block" href="logout">Logout</a>
+        </div>
+    )
+}
+
+export default AdminNav
